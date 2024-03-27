060786a293b940d31c4f40d30ec03f66299beea6
jayo14 <98849557+jayo14@users.noreply.github.com>
2024-03-27 18:27:55 +0100

    "Added view for making transactions and passed transactions object to transaction\_list template"
    This commit adds a new view function `make_transaction` to handle POST requests for making transactions. Additionally, it modifies the existing `transaction_list` view function to pass the `transactions` object to the template, which was missing in the previous version.

--- Yomei-BankApp-master/Transactions/views.py
+++ Yomei-BankApp-master/Transactions/views.py
@@ -5,4 +5,9 @@ from .forms import TransactionForm
 # Create your views here.
 def transaction_list(request):
     transactions = Transaction.objects.all().order_by('-timestamp')[:5]
-    return render(request, 'transactions_.html', {'transactions':}
+    return render(request, 'transactions_.html', {'transactions':transactions})
+
+def make_transaction(request):
+    if request.method  == 'POST':
+        form = TransactionForm(request.POST)
+        
