fad0be21c63f2caf50a00842cde23a7314d6f969
jayo14 <98849557+jayo14@users.noreply.github.com>
2024-03-27 19:54:39 +0100

    "Add account detail view and retrieve associated transactions"
    This commit adds a new view for account details and retrieves all transactions associated with the specified account number. The new view is called 'account\_detail' and takes 'account\_number' as a parameter. It retrieves the account object using the 'Account' model and filters all transactions associated with the account using the 'Transaction' model.

--- Yomei-BankApp-master/Account/views.py
+++ Yomei-BankApp-master/Account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from Transactions.models import Transaction
+from .models import Account
 
 # Create your views here.
+def account_detail(request, account_number):
+    account = Account.objects.get(account_number = account_number)
+
+    transactions = Transaction.objects.filter(account = account)
+    return render()
