fdee6732de89f672ab70ec0a12c7495618b8faf7
jayo14 <98849557+jayo14@users.noreply.github.com>
2024-03-27 18:34:11 +0100

    "Add Transaction model with associated fields and methods; import Account model"
    This diff adds a new model called Transaction to the Transactions app, which includes fields for account, amount, type, transaction type, timestamp, and description. The Transaction model also includes a Meta class to specify the database table name and a \_\_str\_\_ method to provide a string representation of the object. Additionally, a static method get\_recent\_transactions is added to retrieve a specified number of recent transactions for a given account. The Account model from the Account app is also imported for use in the Transaction model.

--- Yomei-BankApp-master/Transactions/models.py
+++ Yomei-BankApp-master/Transactions/models.py
@@ -1,4 +1,29 @@
 from django.db import models
-#from Account.models import Account
+from Account.models import Account
 
 # Create your models here.
+class Transaction(models.Model):
+    TRANS_TYPE_CHOICES = (
+       ('deposit', 'Deposit'),
+       ('withdrawal', 'Withdrawal'),
+       ('transfer', 'Transfer'),
+       ('payment', 'Payment'),
+       ('other', 'Other'),
+    )
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    type = models.CharField(max_length=30)
+    trans_type = models.CharField(max_length=20, choices=TRANS_TYPE_CHOICES)
+    timestamp = models.DateTimeField(add_now_add=True)
+    description = models.CharField(max_length=255, blank=True)
+    
+    class Meta:
+        db_table = 'transactions'
+
+    def __str__(self):
+        return f"{self.trans_type} - {self.amount}"
+
+    @staticmethod
+    def get_recent_transactions(account, num_transactions=5):
+        return Transaction.objects.filter(account=account).order_by('-timestamp')[:num_transactions]
